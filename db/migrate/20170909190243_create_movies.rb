9c4346ef627e3bd00b5d339d2b535faaede47d15
HunterMcAfee <huntmac10@gmail.com>
2017-09-09 15:11:40 -0400

    Added MovieList model and Movie model

---
+++ db/migrate/20170909190243_create_movies.rb
@@ -0,0 +1,19 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :genres
+      t.string :homepage
+      t.integer :movie_id
+      t.string :original_title
+      t.string :overview
+      t.string :poster_path
+      t.string :release_date
+      t.integer :runtime
+      t.string :tagline
+      t.boolean :video
+      t.integer :vote_average
+
+      t.timestamps
+    end
+  end
+end
