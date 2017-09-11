ad43eea24e90e2eccf59dcf6a2623c553a0cca93
HunterMcAfee <huntmac10@gmail.com>
2017-09-11 17:50:21 -0400

    Movie now displays information on the page

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -32,24 +32,20 @@ class Movie extends Component {
         } catch (err) {
             console.log(err);
         }
-        
     }
 
-    // _fetchMovieAPI = async () => {
-    //     console.log(this.state.movieDB)
-    //     try {
-    //         const res = await axios.get(`https://api.themoviedb.org/3/movie/${this.state.movieDB.movie_id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-    //         this.setState({movie: res.data.results})
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // }
-
     render() {
-        const id = this.props.match.params.id
+        const id = this.props.match.params.movie_list_id
         return (
             <div>
-              
+                <h2>Title: {this.state.movie.original_title}</h2>
+                <div>Tagline: {this.state.movie.tagline}</div>
+                <div>Overview: {this.state.movie.overview}</div>
+                <div>Release Date: {this.state.movie.release_date}</div>
+                <div>Budget: {this.state.movie.budget}</div>
+                <div>Rating: {this.state.movie.vote_average}</div>
+                <div>Home Page: {this.state.movie.homepage}</div>
+                <div>Runtime: {this.state.movie.runtime}</div>
                 <Link to={`/movie_lists/${id}`}><button>Back</button></Link>
             </div>
         );
