1b7919f59440e7eb97999d95c26db855898f575c
HunterMcAfee <huntmac10@gmail.com>
2017-09-17 20:50:49 -0400

    Added validations for show list and movie list

--- app/models/movie_list.rb
+++ app/models/movie_list.rb
@@ -1,4 +1,7 @@
 class MovieList < ApplicationRecord
     has_many :movies, dependent: :destroy
     belongs_to :user
+    validates :title, :length => { :minimum => 1, :maximum => 25  }
+    validates :category, :length => { :minimum => 1, :maximum => 25  }
+    validates :description, :length => { :minimum => 1, :maximum => 120  }
 end
--- app/models/show_list.rb
+++ app/models/show_list.rb
@@ -1,3 +1,6 @@
 class ShowList < ApplicationRecord
     has_many :shows, dependent: :destroy
+    validates :title, :length => { :minimum => 1, :maximum => 25  }
+    validates :category, :length => { :minimum => 1, :maximum => 25  }
+    validates :description, :length => { :minimum => 1, :maximum => 120  }
 end
